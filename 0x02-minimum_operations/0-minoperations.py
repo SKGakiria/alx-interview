f9cedf1b10ba52a85ce74b1527da966291facb5c
SKGakiria <swgakiria@gmail.com>
2023-10-10 01:43:22 -0700

    Added Task 0 - Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module containing function to calculate the fewest number of
+operations needed to achieve exactly n 'H' characters"""
+
+
+def minOperations(n):
+    """Function returns integer with minimum number of operations
+    required to obtain exactly n 'H' characters in a file"""
+    operations = 0
+    min_operations = 2
+
+    for min_operations in range(2, n + 1):
+        while n % min_operations == 0:
+            operations += min_operations
+            n //= min_operations
+
+    return operations
