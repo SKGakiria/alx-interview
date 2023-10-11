0a6717652363f4fe8542a46e774342a052ecc3ae
SKGakiria <swgakiria@gmail.com>
2023-10-11 09:23:54 -0700

    Updated solution to Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,9 +9,10 @@ def minOperations(n):
     operations = 0
     min_operations = 2
 
-    for min_operations in range(2, n + 1):
+    while n > 1:
         while n % min_operations == 0:
             operations += min_operations
-            n //= min_operations
+            n /= min_operations
+        min_operations += 1
 
     return operations
