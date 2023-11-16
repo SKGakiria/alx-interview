a5c21e36df4d07f3283efea0e47c1613c48130a2
SKGakiria <swgakiria@gmail.com>
2023-11-16 07:18:48 -0800

    Added Task 0 - Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,7 +12,7 @@ def rotate_2d_matrix(matrix):
             matrix[j][i] = temp
 
     for i in range(n):
-        for j in range(int(n / 2)):
+        for j in range(int(n // 2)):
             temp = matrix[i][j]
             matrix[i][j] = matrix[i][n-1-j]
             matrix[i][n-1-j] = temp
