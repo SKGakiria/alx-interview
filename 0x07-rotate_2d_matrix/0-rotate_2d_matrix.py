e1eaa8af308b0b65ce232c2698c78c06fcfbdf9e
SKGakiria <swgakiria@gmail.com>
2023-11-16 01:16:58 -0800

    Added README.md file

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The 2D Matrix module"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates a 2D matrix 90 degrees clockwise"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
