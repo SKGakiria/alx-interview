43b4ff9648aa4eacf5a4fbcb5d35866ff9db882d
SKGakiria <swgakiria@gmail.com>
2023-11-22 04:05:41 -0800

    Added Task 0 - Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""The Change making module"""
+
+
+def makeChange(coins, total):
+    """Determines fewest number of coins needed to meet a given amount"""
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coins_count = 0
+    for c in coins:
+        if total % c == 0:
+            coins_count += total // c
+            return coins_count
+        if total - c >= 0:
+            coins_count += total // c
+            total = total % c
+        else:
+            coins_count += 1
+            total -= c
+            if total == 0:
+                break
+    if total > 0:
+        return -1
+    return coins_count
