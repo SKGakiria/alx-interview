67821654f0f7008b089f6d593d41016dbef4cccc
SKGakiria <swgakiria@gmail.com>
2023-12-06 10:16:03 -0800

    Updated code for Task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,40 +2,31 @@
 """The Prime Game module"""
 
 
-def primes(n):
-    """Return list of prime numbers"""
-    prime = [True] * (n + 1)
-    prime[0] = prime[1] = False
-    prime_nums = []
-
-    for num in range(2, int(n**0.5) + 1):
-        if prime[num]:
-            prime_nums.append(num)
-            for i in range(num * num, n + 1, num):
-                prime[i] = False
-
-    for num in range(int(n**0.5) + 1, n + 1):
-        if prime[num]:
-            prime_nums.append(num)
-
-    return prime_nums
-
-
 def isWinner(x, nums):
     """Determines the winner of a prime game session"""
-    if x is None or nums is None or x == 0 or nums == []:
+    if x < 1 or not nums:
         return None
 
-    Maria = Ben = 0
-    for n in nums:
-        prime_nums = primes(n)
-        if len(prime_nums) % 2 == 0:
-            Ben += 1
-        else:
-            Maria += 1
+    Maria, Ben = 0, 0
 
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
-    return None
+    def sieve(n):
+        """Return list of prime numbers"""
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+
+        for num in range(2, int(n**0.5) + 1):
+            if is_prime[num]:
+                for multiple in range(num * num, n + 1, num):
+                    is_prime[multiple] = False
+
+        return is_prime
+
+    for round_limit in nums[:x]:
+        primes = sieve(round_limit)
+        primes_count = sum(1 for prime in primes if prime)
+        Ben += primes_count % 2 == 0
+        Maria += primes_count % 2 == 1
+
+    if Maria == Ben:
+        return None
+    return 'Maria' if Maria > Ben else 'Ben'
